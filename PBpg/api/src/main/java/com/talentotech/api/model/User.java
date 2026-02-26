7f2c68f28c27a28f9b9bf42294b463b9d6a22521
Tanilse <tanilsec@gmail.com>
2026-02-26 17:30:50 -0500

    Initial backend structure

---
+++ PBpg/api/src/main/java/com/talentotech/api/model/User.java
@@ -0,0 +1,14 @@
+package com.talentotech.api.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "users")
+public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column (nullable = false, unique = true)
+    private String username;
+    @Column (nullable = false, unique = true)
+    private String email;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BusinessTracker
+Comprehensive web application that allows the visualization and management of data on entrepreneurship and innovation at a global and local level. Using object-oriented programming principles with Java, relational databases, and modern web technologies.
